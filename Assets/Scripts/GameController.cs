3edd79059f15ebba30b34eedf0c328f66c5e16c1
emirhanakturk <emirhanakturk@gmail.com>
2021-05-05 15:17:24 +0300

    Drawing the walls with code and marking on the matrix is ​​complete.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,28 +6,33 @@ public class GameController : MonoBehaviour
 {
     public enum TileStatus{
         Empty = 0,
-        Painted = 1
+        Wall = 1,
+        Painted = 2,
     }
+
+    [SerializeField]
+    MeshRenderer planeMeshRender;
 
     //x = 0, y = 0 sol alt
     //MxN matris için
     // arr[m][n]
     // m = M - y - 1
     // n = x
+    [Tooltip("Number of rows and columns of the matrix")]
     [SerializeField] 
     int M, N;
 
+    [Header("Cubes")]
     [SerializeField] 
-    GameObject cube;
+    GameObject playerCube;
+    [SerializeField]
+    GameObject wallCube;
 
     [SerializeField] 
     Transform parentCubes;
 
     [SerializeField] 
-    List<Vector2Int> points;
-
-    [SerializeField]
-    MeshRenderer planeMeshRender;
+    List<Vector2Int> turningPoints;
 
     TileStatus[ , ] status;
 
@@ -37,7 +42,8 @@ public class GameController : MonoBehaviour
         status = new TileStatus[M, N];
 
         GridEditor();
-        DrawLines();
+        DrawWalls();
+        //DrawLines(turningPoints, playerCube);
     }
 
     private void GridEditor()
@@ -47,26 +53,72 @@ public class GameController : MonoBehaviour
         planeGridMaterial.mainTextureScale = new Vector2(M, N);
     }
 
-    private void DrawLines()
+    private void DrawWalls()
+    {
+        List<Vector2Int> cornerPoints = new List<Vector2Int>();
+        List<Vector2Int> cornerMatrixIndexs = new List<Vector2Int>();
+
+        AddCornerPoints(cornerPoints, cornerMatrixIndexs);
+
+        DrawLines(cornerPoints, wallCube);
+
+        PrintMatrix();
+    }
+
+    private void AddCornerPoints(List<Vector2Int> cornerPoints, List<Vector2Int> cornerMatrixIndexs)
+    {
+        Vector2Int point = ConvertMatrixIndexToPosition(new Vector2Int(0, 0));
+        cornerPoints.Add(point);
+        cornerMatrixIndexs.Add(new Vector2Int(0, 0));
+
+        point = ConvertMatrixIndexToPosition(new Vector2Int(0, N - 1));
+        cornerPoints.Add(point);
+        cornerMatrixIndexs.Add(new Vector2Int(0, N - 1));
+
+        point = ConvertMatrixIndexToPosition(new Vector2Int(M - 1, N - 1));
+        cornerPoints.Add(point);
+        cornerMatrixIndexs.Add(new Vector2Int(M - 1, N - 1));
+
+        point = ConvertMatrixIndexToPosition(new Vector2Int(M - 1, 0));
+        cornerPoints.Add(point);
+        cornerMatrixIndexs.Add(new Vector2Int(M - 1, 0));
+
+        point = ConvertMatrixIndexToPosition(new Vector2Int(0, 0));
+        cornerPoints.Add(point); // for the last wall
+        cornerMatrixIndexs.Add(new Vector2Int(0, 0));
+    }
+
+    private void DrawLines(List<Vector2Int>points, GameObject cube)
     {
         for (int i = 0; i < (points.Count - 1); ++i)
         {
-            DrawLine(points[i], points[i + 1]);
+            DrawLine(points[i], points[i + 1], cube);
         }
     }
 
-    private void DrawLine(Vector2Int p1, Vector2Int p2)
+    private void DrawLine(Vector2Int p1, Vector2Int p2, GameObject cube)
     {
         Vector2Int point = Vector2Int.zero;
         GameObject newCube;
 
         if ( p1.x == p2.x)
         {
-            int min = p1.y < p2.y ? p1.y : p2.y;
-            int max = p1.y > p2.y ? p1.y : p2.y;
+            int upperBound;
+            int lowerBound;
+            if (p2.y < p1.y)
+            {
+                ++p2.y;
+                upperBound = p1.y;
+                lowerBound = p2.y;
+            }
+            else
+            {
+                --p2.y;
+                upperBound = p2.y;
+                lowerBound = p1.y;
+            }
 
-            // todo fix repetition of vertex values.
-            for (int i = min + 1; i <= max; ++i) 
+            for (int i = lowerBound ; i <= upperBound; ++i) 
             {
                 newCube = Instantiate(cube, new Vector3(p1.x, 0, i), Quaternion.identity);
                 newCube.transform.parent = parentCubes;
@@ -74,17 +126,33 @@ public class GameController : MonoBehaviour
                 point.x = p1.x;
                 point.y = i;
 
-                Vector2Int matrixIndex = ConvertPositionToMatris(point);
-                status[matrixIndex.x, matrixIndex.y] = TileStatus.Painted;
+                Vector2Int matrixIndex = ConvertPositionToMatrixIndex(point);
+
+                if (cube.tag == "PlayerCube")
+                    status[matrixIndex.x, matrixIndex.y] = TileStatus.Painted;
+
+                else if (cube.tag == "WallCube")
+                    status[matrixIndex.x, matrixIndex.y] = TileStatus.Wall;
             }
         }
         else if(p1.y == p2.y)
         {
-            int min = p1.x < p2.x ? p1.x : p2.x;
-            int max = p1.x > p2.x ? p1.x : p2.x;
+            int upperBound;
+            int lowerBound;
+            if (p2.x < p1.x)
+            {
+                ++p2.x;
+                upperBound = p1.x;
+                lowerBound = p2.x;
+            }
+            else
+            {
+                --p2.x;
+                upperBound = p2.x;
+                lowerBound = p1.x;
+            }
 
-            // todo fix repetition of vertex values.
-            for (int i = min + 1; i <= max; ++i) 
+            for (int i = lowerBound; i <= upperBound; ++i) 
             {
                 newCube = Instantiate(cube, new Vector3(i, 0, p1.y), Quaternion.identity);
                 newCube.transform.parent = parentCubes;
@@ -92,22 +160,41 @@ public class GameController : MonoBehaviour
                 point.x = i;
                 point.y = p1.y;
 
-                Vector2Int matrixIndex = ConvertPositionToMatris(point);
-                status[matrixIndex.x, matrixIndex.y] = TileStatus.Painted;
+                Vector2Int matrixIndex = ConvertPositionToMatrixIndex(point);
+
+                if (cube.tag == "PlayerCube")
+                    status[matrixIndex.x, matrixIndex.y] = TileStatus.Painted;
+
+                else if (cube.tag == "WallCube")
+                    status[matrixIndex.x, matrixIndex.y] = TileStatus.Wall;
             }
         }
     }
 
-    private Vector2Int ConvertPositionToMatris(Vector2Int point)
+    private Vector2Int ConvertPositionToMatrixIndex(Vector2Int point)
     {
-        Vector2Int matrixIndex = new Vector2Int(M - point.y - 1 , point.x);
-
+        // (m, n) = (M - y - 1 , x)   // (MxN matrix) 
+        Vector2Int matrixIndex = new Vector2Int(M - point.y - 1, point.x);
         return matrixIndex;
     }
-
-    // Update is called once per frame
-    private void Update()
+    
+    private Vector2Int ConvertMatrixIndexToPosition(Vector2Int matrixIndex)
     {
+        // (x, y) = (n, M - m - 1) // (MxN matrix) 
+        Vector2Int positionIndex = new Vector2Int(matrixIndex.y, M - matrixIndex.x - 1 );
+        return positionIndex;
+    }
 
+    private void PrintMatrix()
+    {
+        for (int i = 0; i < M; ++i)
+        {
+            Debug.Log((i + 1) + ". satır:");
+
+            for (int j = 0; j < N; ++j)
+                Debug.Log(status[i, j]);
+
+            Debug.Log("####################################");
+        }
     }
 }
